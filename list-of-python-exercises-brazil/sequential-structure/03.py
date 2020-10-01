cabbfb00e46fe0d682c7c41b83a8b7b9d9597fa2
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-10-01 16:16:23 -0300

    exercise: 03- Make a program that asks for two numbers and print the sum

---
+++ list-of-python-exercises-brazil/sequential-structure/03.py
@@ -0,0 +1,7 @@
+# Faça um Programa que peça dois números e imprima a soma.
+num1 = float(input('Informe um número: '))
+num2 = float(input('Informe outro número: '))
+
+result = num1 + num2
+
+print('O resultado da soma é:', result)
