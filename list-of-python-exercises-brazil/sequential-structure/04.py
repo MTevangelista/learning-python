3262b658f7323d0581e8d9c9fb8ef636faecb398
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-10-01 16:28:16 -0300

    exercise: 04- Make a program that asks for the 4 bi-monthly notes and show the media

---
+++ list-of-python-exercises-brazil/sequential-structure/04.py
@@ -0,0 +1,10 @@
+# Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+note1 = float(input('Informe a sua primeira nota: '))
+note2 = float(input('Informe a sua segunda nota: '))
+note3 = float(input('Informe a sua terceira nota: '))
+note4 = float(input('Informe a sua quarta nota: '))
+
+media = (note1 + note2 + note3 + note4) / 4
+
+print('A média é:', media)
