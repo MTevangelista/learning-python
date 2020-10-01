e6e9ea9b248ddeaa819e010d69bbf047f18771e1
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-10-01 15:01:29 -0300

    add introduction

---
+++ python-guide/introduction/main.py
@@ -0,0 +1,12 @@
+nota1 = 2
+nota2 = 3
+nota3 = 3
+
+media = (nota1 + nota2 + nota3) / 3
+
+if(media >= 7):
+    print('Aprovado')
+elif(media >= 5):
+    print('Recuperação')
+else:
+    print('Reprovado')
