f1bfe3d085d8caec83a86b24bd0c080a5bc0688a
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-10-01 17:36:53 -0300

    exercise: 07- Make a Program that calculates the area of a square, then show twice this area to the user

---
+++ list-of-python-exercises-brazil/sequential-structure/07.py
@@ -0,0 +1,7 @@
+# Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+lado = float(input('Informe o lado do quadrado: '))
+area = lado ** 2
+
+print('A área do quadrado é:', area)
+print('O dobro desta área é:', area * 2)
