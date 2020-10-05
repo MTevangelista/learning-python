16be1145e101e2adc29ee61385d75ab12e454bfa
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-10-05 19:40:54 -0300

    Make a program that asks the temperature in degrees Celsius, transform and show in degrees Fahrenheit

---
+++ list-of-python-exercises-brazil/sequential-structure/10.py
@@ -0,0 +1,7 @@
+# Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Fahrenheit.
+
+c = float(input('Informe a temperatura em graus celsius: '))
+
+result = c * 1.8 + 32
+
+print('A temperatura em graus fahrenheit é:', result)
