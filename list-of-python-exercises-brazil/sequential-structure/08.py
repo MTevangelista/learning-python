1db7555b18413538a7a6d6db5859aa20aa9634f4
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-10-05 09:58:48 -0300

    Make a program that asks how much you earn per hour and the number of hours worked in the month. Calculate and show the total of your salary for that month

---
+++ list-of-python-exercises-brazil/sequential-structure/08.py
@@ -0,0 +1,7 @@
+# Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+salarioPorHora = float(input('Quando você ganha por hora? '))
+horasTrabalhadasNoMes = float(input('Informe as horas trabalhadas no mês: '))
+
+result = salarioPorHora * horasTrabalhadasNoMes
+
+print('O total do sálario é:', result)
