682629d94766d1ce6b9fd48f40ea251dfae71618
Matheus Evangelista <matheusevangelistadev@gmail.com>
2020-10-07 10:57:19 -0300

    Exercise 06 of the decision structure of the python exercise list Brazil

---
+++ list-of-python-exercises-brazil/decision-structure/06.py
@@ -0,0 +1,13 @@
+# Faça um Programa que leia três números e mostre o maior deles.
+
+n1 = float(input('Informe o primeiro número:\n'))
+n2 = float(input('Informe o segundo número:\n'))
+n3 = float(input('Informe o terceiro número:\n'))
+
+if n1 > n2 and n1 > n3:
+    print(f'O maior é: {n1}')
+elif n2 > n1 and n2 > n3:
+    print(f'O maior é: {n2}')
+else:
+    print(f'O maior é: {n3}')
+print('FIM')
